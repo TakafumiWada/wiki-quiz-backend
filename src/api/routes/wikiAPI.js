d54d1367f8a557e07cd0dde6ae4f03c8adfef12e
WadaTakafumi <wdtk2525@gmail.com>
2021-06-16 11:33:34 +0900

    Add:corsの追加

--- src/api/routes/wikiAPI.js
+++ src/api/routes/wikiAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello~");
+  res.json({ message: "Hello~" });
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express"),
   app = express(),
+  cors = require("cors"),
   port = process.env.PORT || 3000;
+
+app.use(cors());
 
 const wikiAPIRouter = require("./api/routes/wikiAPI");
 app.use(wikiAPIRouter);
